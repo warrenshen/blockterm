b165871fddcd1c910d65ddbe02ea5d184c5c6255
warrenshen <warrenzshen@gmail.com>
2017-11-01 21:45:15 -0700

    Create subreddit model

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: subreddits
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Subreddit < ApplicationRecord
 end
