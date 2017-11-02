26c0f0afb78990f59658b948ce07b5adf94d255d
warrenshen <warrenzshen@gmail.com>
2017-11-01 22:25:07 -0700

    Create subreddit type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,22 @@
-Types::QueryType = GraphQL::ObjectType.define do
-  name "Query"
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
+module Types
+  QueryType = GraphQL::ObjectType.define do
+    name "Query"
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
+    # TODO: remove me
+    field :testField, types.String do
+      description "An example field added by the generator"
+      resolve ->(obj, args, ctx) {
+        "Hello World!"
+      }
+    end
+
+    field :subreddit, Types::SubredditType do
+      description "The subreddit that corresponds to the given subreddit id"
+      resolve ->(obj, args, ctx) {
+        Subreddit.find(args[:id])
+      }
+    end
   end
 end
