5b6ee85dfd77e43b928a374f23be0d433355e82c
warrenshen <warrenzshen@gmail.com>
2017-11-01 22:53:55 -0700

    Add a unique index on subreddits name column

--- app/models/subscription_count.rb
+++ app/models/subscription_count.rb
@@ -9,6 +9,10 @@
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
+# Indexes
+#
+#  index_subscription_counts_on_subreddit_id  (subreddit_id)
+#
 # Foreign Keys
 #
 #  fk_rails_...  (subreddit_id => subreddits.id)
--- db/migrate/20171102043743_create_subreddits.rb
+++ db/migrate/20171102043743_create_subreddits.rb
@@ -1,8 +1,10 @@
 class CreateSubreddits < ActiveRecord::Migration[5.0]
   def change
     create_table :subreddits do |t|
-      t.string :name, :null => false
+      t.string :name, null: false
       t.timestamps
+
+      t.index :name, unique: true
     end
   end
 end
