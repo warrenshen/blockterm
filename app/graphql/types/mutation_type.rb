b5181b6e7ff8327ac0545ec4b8dd70febdf8b907
warrenshen <warrenzshen@gmail.com>
2017-11-04 20:54:41 -0700

    Create create subreddit and create subscription count mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,12 +1,32 @@
 module Types
   MutationType = GraphQL::ObjectType.define do
-    name "Mutation"
+    name 'Mutation'
 
-    # TODO: Remove me
-    field :testField, types.String do
-      description "An example field added by the generator"
-      resolve ->(obj, args, ctx) {
-        "Hello World!"
+    field :createSubreddit, Types::SubredditType do
+      description 'Creates a subreddit'
+
+      argument :name, !types.String
+
+      resolve -> (obj, args, ctx) {
+        Subreddit.find_or_create_by(
+          name: args[:name]
+        )
+      }
+    end
+
+    field :createSubscriptionCount, Types::SubscriptionCountType do
+      description 'Creates a subscription count'
+
+      argument :subreddit_id, !types.ID
+      argument :count, !types.Int
+      argument :when, !types.String
+
+      resolve -> (obj, args, ctx) {
+        SubscriptionCount.find_or_create(
+          subreddit_id: args[:subreddit_id],
+          count: args[:count],
+          when: args[:when],
+        )
       }
     end
   end
