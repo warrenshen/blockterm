556a4083bcc21651f80bfffd6555486b4a30e608
warrenshen <warrenzshen@gmail.com>
2017-11-07 23:47:09 -0800

    Create keyword model

---
+++ app/models/keyword.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: keywords
+#
+#  id         :integer          not null, primary key
+#  token_id   :integer          not null
+#  word       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_keywords_on_token_id  (token_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (token_id => tokens.id)
+#
+
+class Keyword < ApplicationRecord
+  belongs_to :token
+end
