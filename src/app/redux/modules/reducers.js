6a1f119aa25edb607897e6d26af5282aba0aaed3
warrenshen <warrenzshen@gmail.com>
2017-11-15 21:35:34 -0800

    Create plot constants, reducer, actions

--- src/app/redux/modules/reducers.js
+++ src/app/redux/modules/reducers.js
@@ -1,10 +1,12 @@
 // @flow weak
 
-import { combineReducers }      from 'redux';
-import { routerReducer }        from 'react-router-redux';
-import { apolloClient }         from '../../services/apollo';
+import { combineReducers } from 'redux';
+import { routerReducer }   from 'react-router-redux';
+import { apolloClient }    from '../../services/apollo';
+import plots               from './plots';
 
 const appReducers = {
+  plots,
 };
 
 // combine reducers -> createStore reducer
