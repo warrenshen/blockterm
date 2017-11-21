9745ce1eda4477593c7a1b0f639f4391f9f8ce99
warrenshen <warrenzshen@gmail.com>
2017-11-21 10:10:48 -0800

    Create token route, container, and view

---
+++ client/src/app/containers/token/Token.js
@@ -0,0 +1,58 @@
+// @flow weak
+
+import { connect }            from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Token }              from '../../views'
+import gql                    from 'graphql-tag';
+import { graphql }            from 'react-apollo';
+
+/* -----------------------------------------
+  GraphQL - Apollo client
+ ------------------------------------------*/
+
+const TokenQuery = gql`
+ query ($id: ID!) {
+    tokenById(id: $id) {
+      id
+      shortName
+      longName
+    }
+  }
+`;
+
+const TokenWithQuery = graphql(
+  TokenQuery,
+  {
+    options: ({ match }) => {
+      return {
+        variables: {
+          id: match.params.id,
+        },
+      };
+    }
+  }
+)(Token);
+
+
+/* -----------------------------------------
+  Redux
+ ------------------------------------------*/
+
+const mapStateToProps = (state) => {
+  return {
+    nightMode: state.globals.nightMode,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+    },
+    dispatch
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TokenWithQuery);
--- client/src/app/views/index.js
+++ client/src/app/views/index.js
@@ -2,3 +2,4 @@
 
 export { default as Home }      from './home/Home';
 export { default as Subreddit } from './subreddit/Subreddit';
+export { default as Token }     from './token/Token';
