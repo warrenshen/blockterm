f2a13362d7910a33559c6ef9bb6fd4b6b88a0419
warrenshen <warrenzshen@gmail.com>
2017-11-22 09:34:09 -0800

    Fix indentation of api functions

--- data/api.py
+++ data/api.py
@@ -3,97 +3,98 @@ import requests
 
 class Api:
 
-  def __init__(api_url):
-    if not api_url:
-      raise Exception('A api url is required')
-    else:
-      self.api_url = api_url
+    def __init__(self, api_url):
+        if not api_url:
+            raise Exception('A api url is required')
+        else:
+            self.api_url = api_url
 
-    def create_subreddit(name, start_date='2017-01-01'):
-	query = { 'query' : '''
-	    mutation {
-	      createSubreddit(name: "%s", startDate: "%s") {
-		id
-	      }
-	    }''' % (name, start_date)
-	}
+    def create_subreddit(self, name, start_date='2017-01-01'):
+        query = { 'query' : '''
+            mutation {
+              createSubreddit(name: "%s", startDate: "%s") {
+                id
+              }
+            }''' % (name, start_date)
+        }
 
-	r = requests.post(url=self.api_url, json=query)
-	response = json.loads(r.text)
-	return response
+        r = requests.post(url=self.api_url, json=query)
+        response = json.loads(r.text)
+        return response
 
-    def create_post_count(subreddit_id, count, timestamp):
-	query = { 'query' : '''
-	    mutation {
-	      createPostCount(subredditId: %s, count: %s, timestamp: "%s") {
-		subredditId
-		count
-		timestamp
-	      }
-	    }''' % (subreddit_id, count, timestamp)
-	}
+    def create_post_count(self, subreddit_name, count, timestamp):
+        query = { 'query' : '''
+            mutation {
+              createPostCount(subredditName: "%s", count: %s, timestamp: "%s") {
+                subredditId
+                count
+                timestamp
+              }
+            }''' % (subreddit_name, count, timestamp)
+        }
 
-	r = requests.post(url=self.api_url, json=query)
-	response = json.loads(r.text)
-	return response
+        r = requests.post(url=self.api_url, json=query)
+        response = json.loads(r.text)
+        return response
 
-    def create_comment_count(subreddit_id, count, timestamp):
-	query = { 'query' : '''
-	    mutation {
-	      createCommentCount(subredditId: %s, count: %s, timestamp: "%s") {
-		subredditId
-		count
-		timestamp
-	      }
-	    }''' % (subreddit_id, count, timestamp)
-	}
+    def create_comment_count(self, subreddit_id, count, timestamp):
+        query = { 'query' : '''
+            mutation {
+              createCommentCount(subredditId: %s, count: %s, timestamp: "%s") {
+                subredditId
+                count
+                timestamp
+              }
+            }''' % (subreddit_id, count, timestamp)
+        }
 
-	r = requests.post(url=self.api_url, json=query)
-	response = json.loads(r.text)
-	return response
+        r = requests.post(url=self.api_url, json=query)
+        response = json.loads(r.text)
+        return response
 
-    def gql_create_active_user_count(subreddit_id, count, timestamp):
-	query = { 'query' : '''
-	    mutation {
-	      createActiveUserCount(subredditId: %s, count: %s, timestamp: "%s") {
-		subredditId
-		count
-		timestamp
-	      }
-	    }''' % (subreddit_id, count, timestamp)
-	}
+    def gql_create_active_user_count(self, subreddit_id, count, timestamp):
+        query = { 'query' : '''
+            mutation {
+              createActiveUserCount(subredditId: %s, count: %s, timestamp: "%s") {
+                subredditId
+                count
+                timestamp
+              }
+            }''' % (subreddit_id, count, timestamp)
+        }
 
-	r = requests.post(url=self.api_url, json=query)
-	response = json.loads(r.text)
-	return response
+        r = requests.post(url=self.api_url, json=query)
+        response = json.loads(r.text)
+        return response
 
-    def gql_update_subreddit_blob(subreddit_id,
-				  post_count_24h=None,
-				  comment_count_24h=None,
-				  active_user_count=None,
-				  subscriber_count=None
-				 ):
-	params = 'id: "%s"' % subreddit_id
-	if post_count_24h:
-	    params += ', postCount24h: %s' % post_count_24h
-	if comment_count_24h:
-	    params += ', commentCount24h: %s' % comment_count_24h
-	if active_user_count:
-	    params += ', activeUserCountNow: %s' % active_user_count
-	if subscriber_count:
-	    params += ', subscriberCountNow: %s' % subscriber_count
+    def gql_update_subreddit_blob(self,
+                                  subreddit_id,
+                                  post_count_24h=None,
+                                  comment_count_24h=None,
+                                  active_user_count=None,
+                                  subscriber_count=None
+                                 ):
+        params = 'id: "%s"' % subreddit_id
+        if post_count_24h:
+            params += ', postCount24h: %s' % post_count_24h
+        if comment_count_24h:
+            params += ', commentCount24h: %s' % comment_count_24h
+        if active_user_count:
+            params += ', activeUserCountNow: %s' % active_user_count
+        if subscriber_count:
+            params += ', subscriberCountNow: %s' % subscriber_count
 
-	query = { 'query': '''
-	    mutation {
-	      updateSubredditBlob(%s) {
-		id
-		name
-		blob
-	      }
-	    }''' % (params)
-	}
+        query = { 'query': '''
+            mutation {
+              updateSubredditBlob(%s) {
+                id
+                name
+                blob
+              }
+            }''' % (params)
+        }
 
-	r = requests.post(url=graphql_url, json=query)
-	response = json.loads(r.text)
-	return response
+        r = requests.post(url=graphql_url, json=query)
+        response = json.loads(r.text)
+        return response
 
