dba38d928c715f38ee615a38fc687764da3c121b
warrenshen <warrenzshen@gmail.com>
2017-11-22 09:34:31 -0800

    Create post counts daily script

---
+++ data/post_counts_daily.py
@@ -0,0 +1,35 @@
+import json
+import praw
+import time
+
+from datetime import datetime
+from praw.models import MoreComments
+
+import secrets
+reddit = praw.Reddit(client_id=secrets.CLIENT_ID, client_secret=secrets.CLIENT_SECRET, user_agent=secrets.USER_AGENT)
+reddit.read_only
+
+import api
+import subreddits
+
+ONE_DAY = 86400
+
+def create_post_count_for_subreddit(subreddit_name, praw_subreddit, start, end):
+    posts = praw_subreddit.submissions(start, end)
+    post_count = len(list(posts))
+    unix_dt = datetime.fromtimestamp(end)
+    date_t = unix_dt.strftime('%Y-%m-%d %H:%M:%S')
+    # TODO: accept endpoint through command line
+    server = api.Api('http://localhost:8080/graphql')
+    response = server.create_post_count(subreddit_name, post_count, date_t)
+    return post_count, response
+
+for subreddit_name in subreddits.SUBREDDITS:
+    praw_subreddit = reddit.subreddit(subreddit_name)
+    # End timestamp is start of today, since we are getting post count for yesterday.
+    end = int(time.mktime(datetime.today().date().timetuple()))
+    start = end - ONE_DAY
+    post_count, response = create_post_count_for_subreddit(subreddit_name, praw_subreddit, start, end)
+    print(response)
+
+
