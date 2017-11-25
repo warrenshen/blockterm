c78cf0f5fc2a6ef7ccfa452f55e580be611811f2
warrenshen <warrenzshen@gmail.com>
2017-11-24 18:16:20 -0800

    Support create active user count by subreddit name

--- server/app/graphql/types/mutation_type.rb
+++ server/app/graphql/types/mutation_type.rb
@@ -5,13 +5,32 @@ module Types
     field :createActiveUserCount, Types::ActiveUserCountType do
       description 'Creates a active user count'
 
-      argument :subredditId, !types.ID
+     argument :subredditId, types.ID
+      argument :subredditName, types.String
       argument :count, !types.Int
       argument :timestamp, !types.String
 
       resolve -> (obj, args, ctx) {
+        if args[:subredditId].nil? && args[:subredditName].nil?
+          return GraphQL::ExecutionError.new(
+            "One of 'subredditId' or 'subredditName' params is required"
+          )
+        end
+
+        if !args[:subredditId].nil?
+          subreddit_id = args[:subredditId]
+        else
+          subreddit = Subreddit.find_by(name: args[:subredditName])
+          if subreddit.nil?
+            return GraphQL::ExecutionError.new(
+              "No subreddit found for given 'subredditId' or 'subredditName'"
+            )
+          end
+          subreddit_id = subreddit.id
+        end
+
         ActiveUserCount.create(
-          subreddit_id: args[:subredditId],
+          subreddit_id: subreddit_id,
           count: args[:count],
           timestamp: args[:timestamp],
         )
