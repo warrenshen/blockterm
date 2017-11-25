4944d3344a528d4a9fa302575dd916b47a90bcb6
warrenshen <warrenzshen@gmail.com>
2017-11-25 12:49:07 -0800

    Update dispatcher script to account for configs path

--- data/dispatcher.py
+++ data/dispatcher.py
@@ -2,28 +2,30 @@ from crontab import CronTab
 
 import configs
 
+def get_command_for_script(script_name):
+    return 'python %s/%s' % (configs.CRONTAB_PATH, script_name)
+
 cron_tab = CronTab(user=configs.CRONTAB_USER)
 cron_tab.remove_all()
 cron_tab.write()
 
 cron_tab = CronTab(user=configs.CRONTAB_USER)
-job = cron_tab.new(command='python active_user_and_subscribers_counts.py')
+job = cron_tab.new(command=get_command_for_script('sync_comments.py'))
+job.minute.every(10)
+cron_tab.write()
+
+cron_tab = CronTab(user=configs.CRONTAB_USER)
+job = cron_tab.new(command=get_command_for_script('active_user_and_subscriber_counts.py'))
 job.minute.every(15)
 cron_tab.write()
 
 cron_tab = CronTab(user=configs.CRONTAB_USER)
-job = cron_tab.new(command='python post_counts.py')
+job = cron_tab.new(command=get_command_for_script('post_counts.py'))
 job.day.every(1)
 cron_tab.write()
 
 cron_tab = CronTab(user=configs.CRONTAB_USER)
-job = cron_tab.new(command='python sync_comments.py')
-job.minute.every(10)
-cron_tab.write()
-
-
-cron_tab = CronTab(user=configs.CRONTAB_USER)
-job = cron_tab.new(command='python comment_counts.py')
+job = cron_tab.new(command=get_command_for_script('comment_counts.py'))
 job.day.every(1)
 cron_tab.write()
 
