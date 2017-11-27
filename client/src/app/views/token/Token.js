4944caf5b09fe978a58b21bb9ca9c88ac6297371
warrenshen <warrenzshen@gmail.com>
2017-11-26 20:03:56 -0800

    Create token page related components

--- client/src/app/views/token/Token.js
+++ client/src/app/views/token/Token.js
@@ -6,7 +6,9 @@ import React, {
 import PropTypes           from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 import { Link }            from 'react-router-dom';
-import El from '../../components/El';
+import SubredditWidget     from '../../components/SubredditWidget';
+import TokenBody           from '../../components/TokenBody';
+import El                  from '../../components/El';
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -22,6 +24,24 @@ const styles = StyleSheet.create({
   container: {
     gridColumn: '3 / 7',
   },
+  header: {
+    padding: '24px 0px',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  banner: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  names: {
+    paddingLeft: '12px',
+    display: 'flex',
+  },
+  subreddits: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: '12px',
+  },
 });
 
 class Token extends PureComponent {
@@ -34,6 +54,36 @@ class Token extends PureComponent {
     nightMode: PropTypes.bool.isRequired,
   };
 
+  renderSubreddits(subreddits)
+  {
+    const {
+      nightMode,
+    } = this.props;
+
+    if (subreddits.length > 0)
+    {
+      return (
+        <div className={css(styles.subreddits)}>
+          <El
+            nightMode={nightMode}
+            type={'h5'}
+          >
+            Related subreddits
+          </El>
+          {
+            subreddits.map((subreddit) => (
+              <SubredditWidget
+                key={subreddit.id}
+                nightMode={nightMode}
+                subreddit={subreddit}
+              />
+            ))
+          }
+        </div>
+      );
+    }
+  }
+
   renderToken(token)
   {
     const {
@@ -43,13 +93,29 @@ class Token extends PureComponent {
     return (
       <div>
         <div className={css(styles.header)}>
-          <El
-            nightMode={nightMode}
-            type={'h2'}
-          >
-            {token.shortName}
-          </El>
+          <div className={css(styles.banner)}>
+            <img src={token.imageUrl} width={96} height={96}></img>
+            <div className={css(styles.names)}>
+              <El
+                nightMode={nightMode}
+                type={'h2'}
+              >
+                {token.longName}
+              </El>
+              <El
+                nightMode={nightMode}
+                type={'h3'}
+              >
+                {token.shortName}
+              </El>
+            </div>
+          </div>
+          {this.renderSubreddits(token.subreddits)}
         </div>
+        <TokenBody
+          token={token}
+          nightMode={nightMode}
+        />
       </div>
     );
   }
