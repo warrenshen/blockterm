822f156da0de2fb83d7302c1b79abfd3d6478b1c
warrenshen <warrenzshen@gmail.com>
2017-11-26 20:03:56 -0800

    Create token page related components

---
+++ client/src/app/components/SubredditWidget.js
@@ -0,0 +1,53 @@
+// @flow weak
+
+import React               from 'react';
+import PropTypes           from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+import { Link }            from 'react-router-dom';
+import El from './El';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: '6px',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  section: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  sectionRight: {
+    paddingLeft: '12px',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+});
+
+const SubredditWidget = ({
+  nightMode,
+  subreddit,
+}) => {
+  console.log(subreddit);
+  return (
+    <div className={css(styles.container)}>
+      <div className={css(styles.section)}>
+        <img src={subreddit.imageUrl} width={48} height={48}></img>
+      </div>
+      <div className={css(styles.section, styles.sectionRight)}>
+        <Link to={`/subreddit/${subreddit.id}`}>
+          <El nightMode={nightMode} type={'span'}>
+            {subreddit.displayName}
+          </El>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+SubredditWidget.propTypes = {
+  nightMode: PropTypes.bool.isRequired,
+  subreddit: PropTypes.object.isRequired,
+};
+
+export default SubredditWidget;
