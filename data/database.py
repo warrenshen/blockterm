fba5c5e2af86183c94b206f2991ad8c50cd3467f
warrenshen <warrenzshen@gmail.com>
2017-12-04 20:20:38 -0800

    Add insert comment method to database

--- data/database.py
+++ data/database.py
@@ -9,3 +9,43 @@ class SQLite3Database:
     def __init__(self, db='test.db'):
         self.conn = sqlite3.connect(db)
         self.cursor = self.conn.cursor()
+        
+    def insert_comment(self,
+                       subreddit_name,
+                       comment_id,
+                       parent_id,
+                       link_id,
+                       body,
+                       created_utc
+                      ):
+        if comment_id.find('t1_') != 0:
+            comment_id = 't1_' + comment_id
+
+        try:
+            self.cursor.execute('''
+                INSERT INTO comments (
+                    comment_id,
+                    parent_id,
+                    subreddit_name,
+                    link_id,
+                    body,
+                    created_utc
+                )
+                VALUES (?, ?, ?, ?, ?, ?)
+            ''', (
+                comment_id,
+                parent_id,
+                subreddit_name,
+                link_id,
+                body,
+                created_utc
+            ))
+        except sqlite3.IntegrityError:
+            raise sqlite3.IntegrityError
+        
+        try:
+            self.conn.commit()
+            return True
+        except:
+            return False
+        
