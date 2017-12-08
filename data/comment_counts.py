a10d71672f6e324323b7bf8880ecc8fd1e9484e5
warrenshen <warrenzshen@gmail.com>
2017-12-08 15:55:01 -0800

    Add update subreddit blob functionality to sync comments script

--- data/comment_counts.py
+++ data/comment_counts.py
@@ -1,35 +1,28 @@
-import json
 import praw
 import time
 
 import secrets
 reddit = praw.Reddit(client_id=secrets.CLIENT_ID, client_secret=secrets.CLIENT_SECRET, user_agent=secrets.USER_AGENT)
 
-import api
+from api import Api
 from database import SQLite3Database
 from logger import logger
 from subreddits import SUBREDDITS
 from utils import unix_timestamp_today, unix_timestamp_to_datetime_string
 
 db = SQLite3Database('comments.db')
-server = api.Api()
+server = Api()
 
 ONE_DAY = 86400
 
 logger.info('Starting comment counts script...')
 
 def create_comment_count_for_subreddit(subreddit_name, start, end):
-	result = db.cursor.execute(
-		'''
-		SELECT COUNT(*)
-		FROM comments
-		WHERE comments.subreddit_name = ?
-		AND comments.created_utc >= ?
-		AND comments.created_utc < ?
-		''',
-		(subreddit_name, start, end)
+	comment_count = db.get_comment_count_for_subreddit(
+		subreddit_name,
+		start,
+		end
 	)
-	comment_count = list(result)[0][0]
 
 	datetime_string = unix_timestamp_to_datetime_string(start)
 	return server.create_comment_count(subreddit_name, comment_count, datetime_string)
