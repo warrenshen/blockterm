ebd9a6a1332c01bf2fa9394fa5a595c366585b5e
warrenshen <warrenzshen@gmail.com>
2017-12-07 23:50:52 -0800

    Fix log path and active user and subscriber count job frequency

--- data/logger.py
+++ data/logger.py
@@ -1,7 +1,11 @@
 import logging
 
+from configs import CRONTAB_PATH
+
+log_path = '%s/log.log' % CRONTAB_PATH
+
 formatter = logging.Formatter(fmt='%(asctime)-15s %(levelname)-8s %(message)s')
-handler = logging.FileHandler('log.log', 'a+')
+handler = logging.FileHandler(log_path, 'a+')
 handler.setFormatter(formatter)
 
 logger = logging.getLogger('blockterm')
