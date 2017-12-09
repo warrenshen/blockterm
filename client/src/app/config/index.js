7d3528ac5965b71990b9bde091342f33a70b378e
warrenshen <warrenzshen@gmail.com>
2017-12-08 17:31:40 -0800

    Fix config to launch apollo network with env based endpoint

--- client/src/app/config/index.js
+++ client/src/app/config/index.js
@@ -1,8 +1,13 @@
 // @flow weak
 
+// Note that NODE_ENV is set in package.json.
+const API_URL = process.env.NODE_ENV == 'dev' ?
+               'http://localhost:9999/graphql' :
+               'http://api.blockterm.com/graphql';
+
 export const appConfig = {
   // apollo client:
   apollo: {
-    networkInterface: 'http://localhost:9999/graphql'
-  }
+    networkInterface: API_URL,
+  },
 };
