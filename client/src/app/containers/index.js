8027f3bde3cac3cc742838d99ca0be93037f7510
warrenshen <warrenzshen@gmail.com>
2017-12-09 18:51:04 -0800

    Create subreddits page - home directs to it now as well

--- client/src/app/containers/index.js
+++ client/src/app/containers/index.js
@@ -1,9 +1,9 @@
 // @flow weak
 
-export { default as App }                 from './app/App';
-export { default as ConnectedNavigationBar } from './navigation/NavigationBar';
-export { default as ConnectedHome }       from './home/Home';
-export { default as ConnectedSubreddit }  from './subreddit/Subreddit';
-export { default as ConnectedToken } from './token/Token';
-export { default as ConnectedTokens } from './Tokens';
+export { default as App }                        from './app/App';
+export { default as ConnectedNavigationBar }     from './navigation/NavigationBar';
+export { default as ConnectedSubreddit }         from './subreddit/Subreddit';
+export { default as ConnectedSubreddits }        from './Subreddits';
 export { default as ConnectedSubredditsCompare } from './SubredditsCompare';
+export { default as ConnectedToken }             from './token/Token';
+export { default as ConnectedTokens }            from './Tokens';
