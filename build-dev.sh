3ac884017d2e61fd14fe4b113b72b16961543b71
Bob <bob@houzz.com>
2017-12-11 16:46:20 -0800

    Change config files
    Change config files to migrate the project to blockterm.

--- build-dev.sh
+++ build-dev.sh
@@ -1,8 +1,8 @@
 echo "Building docker images for client"
-echo "docker build -f deployment/Dockerfile.client -t us.gcr.io/crypto-trends/client ."
-docker build -f deployment/Dockerfile.client -t us.gcr.io/crypto-trends/client .
+echo "docker build -f deployment/Dockerfile.client -t us.gcr.io/blockterm-188823/client ."
+docker build -f deployment/Dockerfile.client -t us.gcr.io/blockterm-188823/client .
 
 echo "Building docker images for server"
-echo "docker build -f deployment/Dockerfile.server -t us.gcr.io/crypto-trends/server ."
-docker build -f deployment/Dockerfile.server -t us.gcr.io/crypto-trends/server .
+echo "docker build -f deployment/Dockerfile.server -t us.gcr.io/blockterm-188823/server ."
+docker build -f deployment/Dockerfile.server -t us.gcr.io/blockterm-188823/server .
 
