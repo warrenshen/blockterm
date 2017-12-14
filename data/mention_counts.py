72314e2fde806e432086ebd5d6c4fe34867b2dc2
warrenshen <warrenzshen@gmail.com>
2017-12-13 22:03:00 -0800

    Set up v1 version of mention counts script

--- data/mention_counts.py
+++ data/mention_counts.py
@@ -1,9 +1,6 @@
-from gensim.utils import simple_preprocess
-
 from api import Api
 from database import SQLite3Database
 from logger import logger
-from reddit import reddit
 from subreddits import SUBREDDITS
 from utils import unix_timestamp_today, unix_timestamp_to_datetime_string
 
@@ -21,31 +18,19 @@ for keyword in keywords:
   keyword_id_to_words[keyword['id']] = keyword['word']
 
 def create_mention_counts_for_subreddit(subreddit_name, keyword_id_to_words, start, end):
-  db = SQLite3Database('comments.db')
-  result = db.cursor.execute(
-    '''
-    SELECT *
-    FROM comments
-    WHERE comments.subreddit_name = ?
-    AND comments.created_utc >= ?
-    AND comments.created_utc < ?
-    ''',
-    (subreddit_name, start, end)
-  )
-  comments = list(result)
-  db.close()
-
-  comment_bodies = [comment[4] for comment in comments if type(comment[4]) is unicode]
-  # A text blob of all comments in given range, separated by a special delimeter.
-  text = ' &&& '.join(comment_bodies)
-  text_lower = text.lower()
-  tokens = simple_preprocess(text_lower)
-
   datetime_string = unix_timestamp_to_datetime_string(start)
 
   for keyword_id, word in keyword_id_to_words.iteritems():
-      count = tokens.count(word.lower())
-      response = server.create_mention_count(subreddit_name, keyword_id, count, datetime_string)
+    db = SQLite3Database('comments.db')
+    mention_count = db.get_keyword_count_for_subreddit(subreddit_name, word, start, end)
+    db.close()
+
+    response = server.create_mention_count(
+      subreddit_name,
+      keyword_id,
+      mention_count,
+      datetime_string
+    )
 
 for subreddit_name in SUBREDDITS:
     # End timestamp is start of today, since we are getting mention count for yesterday.
