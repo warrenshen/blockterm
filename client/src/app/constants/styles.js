72183f3cedde2b40ac211804be9d23a6503a2169
Nick Shen <nickzshen@gmail.com>
2017-12-18 21:35:04 -0800

    Subreddits chart things

--- client/src/app/constants/styles.js
+++ client/src/app/constants/styles.js
@@ -4,8 +4,12 @@ import { StyleSheet } from 'aphrodite';
 export const LIGHTNIGHT = '#0b1010';
 export const SOFTGRAY = '#121515';
 
+export const BLOOMYBLUE = '#2900EA';
+export const LAVENDER = '#6447ED';
 export const GOLD = '#FFD000';
 export const ORANGE = '#FF8000';
+export const PURPLE = '#640BD3';
+export const TRIADORANGE = '#ED7518';
 
 export const STYLES = StyleSheet.create({
   card: {
