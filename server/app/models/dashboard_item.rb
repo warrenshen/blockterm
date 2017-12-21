02527e8e84bd82d525a143711c63ed986c1a6881
warrenshen <warrenzshen@gmail.com>
2017-12-20 18:17:59 -0800

    Create dashboard item model and type and create user type

---
+++ server/app/models/dashboard_item.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: dashboard_items
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  identifier :string           not null
+#  w          :integer          default(0), not null
+#  h          :integer          default(0), not null
+#  x          :integer          default(0), not null
+#  y          :integer          default(0), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_dashboard_items_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
+class DashboardItem < ApplicationRecord
+  belongs_to :user
+end
