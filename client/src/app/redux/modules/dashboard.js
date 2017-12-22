00576583b5764263398106f58beec00339363c20
warrenshen <warrenzshen@gmail.com>
2017-12-22 11:56:26 -0800

    Component fixes on dashboard and token

--- client/src/app/redux/modules/dashboard.js
+++ client/src/app/redux/modules/dashboard.js
@@ -18,7 +18,8 @@ const IDENTIFIER_TO_STATE_MAP = {
 /* -----------------------------------------
   constants
  ------------------------------------------*/
-const REGISTER_ITEM = 'REGISTER_ITEM';
+const CHANGE_DASHBOARD_ITEM_PLOT_RANGE = 'CHANGE_DASHBOARD_ITEM_PLOT_RANGE';
+const REGISTER_DASHBOARD_ITEM = 'REGISTER_DASHBOARD_ITEM';
 
 /* -----------------------------------------
   Reducer
@@ -27,27 +28,50 @@ const initialState = {};
 
 export default function(state = initialState, action)
 {
+  console.log(action);
   switch (action.type)
   {
-    case REGISTER_ITEM:
-      const item = action.value;
-      const {
-        id,
-      } = item;
-
+    case CHANGE_DASHBOARD_ITEM_PLOT_RANGE:
+      var id = action.id;
+      var value = action.value;
+      console.log(id);
+      console.log(value);
       return {
         ...state,
-        [id]: {},
+        [id]: Object.assign({}, state[id], { plotRange: value }),
+      };
+    case REGISTER_DASHBOARD_ITEM:
+      const dashboardItem = action.value;
+      var {
+        id,
+        identifier,
+      } = dashboardItem;
+      const prefix = identifier.substring(0, identifier.lastIndexOf('-'));
+      return {
+        ...state,
+        [id]: Object.assign({}, IDENTIFIER_TO_STATE_MAP[prefix]),
       };
     default:
       return state;
   }
 }
 
-export function registerItem(item)
+export function registerDashboardItem(dashboardItem)
 {
   return {
-    type: REGISTER_ITEM,
-    value: item,
+    type: REGISTER_DASHBOARD_ITEM,
+    value: dashboardItem,
+  };
+}
+
+export function changeDashboardItemPlotRange(dashboardItemId, value)
+{
+  console.log("yello");
+  console.log(dashboardItemId);
+  console.log(value);
+  return {
+    id: dashboardItemId,
+    type: CHANGE_DASHBOARD_ITEM_PLOT_RANGE,
+    value: value,
   };
 }
