318f427957db664ce92276819755db93a1f05aa7
warrenshen <warrenzshen@gmail.com>
2017-12-22 21:58:31 -0800

    Create trading view chart item component

---
+++ client/src/app/components/items/TVChartItem.js
@@ -0,0 +1,53 @@
+// @flow weak
+
+import React, { PureComponent } from 'react';
+import PropTypes           from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  frame: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+class TVChartItem extends PureComponent {
+
+  componentDidMount()
+  {
+    const iframeDocument = this.instance.contentWindow.document;
+
+    var s = iframeDocument.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://s3.tradingview.com/tv.js';
+
+    s.onload = function() {
+      var s = iframeDocument.createElement('script');
+      s.type = 'text/javascript';
+      s.text = 'new TradingView.widget({"autosize": true,"symbol": "NASDAQ:AAPL","interval": "D","timezone": "Etc/UTC","theme": "Light","style": "1","locale": "en","toolbar_bg": "#f1f3f6","enable_publishing": false,"allow_symbol_change": true,"hideideas": true})';
+      iframeDocument.body.appendChild(s);
+    };
+
+    this.instance.contentWindow.document.head.appendChild(s);
+  }
+
+  render()
+  {
+    const {
+      nightMode,
+    } = this.props;
+
+    return (
+      <div className={css(styles.container)}>
+        <iframe className={css(styles.frame)} ref={(el) => this.instance = el}>
+        </iframe>
+      </div>
+    );
+  }
+}
+
+export default TVChartItem;
