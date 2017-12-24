101b8aaca149477a05ffb0350a8e82be78bcf79c
warrenshen <warrenzshen@gmail.com>
2017-12-23 16:52:27 -0800

    Change auth type to have user type in it

--- server/app/graphql/types/auth_type.rb
+++ server/app/graphql/types/auth_type.rb
@@ -2,7 +2,7 @@ module Types
   AuthType = GraphQL::ObjectType.define do
     name 'AuthType'
 
-    field :email, !types.String
     field :authToken, !types.String, property: :auth_token
+    field :user, !Types::UserType
   end
 end
