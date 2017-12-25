fcd3be41c11d072d3690f026d49dbe6bcc693233
warrenshen <warrenzshen@gmail.com>
2017-12-25 00:27:25 -0800

    Add missing moment import

--- client/src/app/components/items/SubredditPostsItem.js
+++ client/src/app/components/items/SubredditPostsItem.js
@@ -3,6 +3,7 @@
 import React               from 'react';
 import PropTypes           from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
+import moment              from 'moment';
 import BarChartWithSelect  from '../BarChartWithSelect';
 import {
   generateCountChartData,
