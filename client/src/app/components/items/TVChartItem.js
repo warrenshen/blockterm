3e82db95c52c3edc2bf2f199cfce6d9fa14ec1e4
warrenshen <warrenzshen@gmail.com>
2017-12-26 09:24:15 -0800

    Refactor tv chart item component for better load time

--- client/src/app/components/items/TVChartItem.js
+++ client/src/app/components/items/TVChartItem.js
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
+    paddingBottom: '15px',
   },
   frame: {
     width: '100%',
@@ -21,69 +22,34 @@ const styles = StyleSheet.create({
 
 class TVChartItem extends PureComponent {
 
-  componentDidMount()
+  render()
   {
-    this.update();
-  }
-
-  componentDidUpdate()
-  {
-    this.update();
-  }
-
-  update() {
     const {
       nightMode,
       value,
     } = this.props;
 
-    const iframeDocument = this.instance.contentWindow.document;
-    while (iframeDocument.body.firstChild) iframeDocument.body.removeChild(iframeDocument.body.firstChild);
-
-    var s = iframeDocument.createElement('script');
-    s.type = 'text/javascript';
-    s.src = 'https://s3.tradingview.com/tv.js';
-
-    const text = `
-      new TradingView.widget({
-        "autosize": true,
-        "symbol": "${value}",
-        "interval": "1",
-        "timezone": "America/Los_Angeles",
-        "theme": "${nightMode ? "Dark" : "Light"}",
-        "style": "1",
-        "locale": "en",
-        "toolbar_bg": "#f1f3f6",
-        "enable_publishing": false,
-        "hide_top_toolbar": true,
-        "withdateranges": true,
-        "save_image": false,
-        "hideideas": true
-      });
-    `;
-
-    s.onload = function() {
-      var s = iframeDocument.createElement('script');
-      s.type = 'text/javascript';
-      s.text = text;
-      iframeDocument.body.appendChild(s);
-      //iframeDocument.body.find(".pane-legend-line.apply-overflow-tooltip.main").css("font-size", "13px");
-      //.apply-overflow-tooltip.main
-      console.log(iframeDocument.querySelector(".pane-legend-line"));
-    };
-
-    this.instance.contentWindow.document.head.appendChild(s);
-  }
-
-  render()
-  {
-    const {
-      nightMode,
-    } = this.props;
+    const url =
+      'https://s.tradingview.com/widgetembed/?' +
+      `symbol=${value}&` +
+      'interval=15&' +
+      'hidesidetoolbar=1&' +
+      'symboledit=0&' +
+      'saveimage=1&' +
+      'toolbarbg=rgba(0,0,0,0)&' +
+      'hideideas=1&' +
+      `theme=${nightMode ? "Dark" : "Light"}&` +
+      'timezone=exchange'
+    ;
+    console.log(url);
 
     return (
       <div className={css(styles.container)}>
-        <iframe className={css(styles.frame, nightMode && styles.nightFrame)} ref={(el) => this.instance = el}>
+        <iframe
+          className={css(styles.frame, nightMode && styles.nightFrame)}
+          src={url}
+          ref={(el) => this.instance = el}
+        >
         </iframe>
       </div>
     );
