9a6515de22a2f73fa898b7ed5ea5bac8842c226c
warrenshen <warrenzshen@gmail.com>
2017-12-28 17:09:24 -0800

    Restructure dashboard container component and fix dashboard changing tabs bug

--- client/src/app/containers/Dashboard.js
+++ client/src/app/containers/Dashboard.js
@@ -68,25 +68,6 @@ function f(identifier, extras)
       }
       return null;
   }
-  // else if (identifier.indexOf('TOKEN-PRICE') === 0)
-  // {
-  //   return `
-  //     ${identifier.replace(/-/g, '')}: tokenById(id: "${id}") {
-  //       id
-
-  //       markets {
-  //         name
-  //         lastPrice
-
-  //         marketTickers {
-  //           id
-  //           value
-  //           timestamp
-  //         }
-  //       }
-  //     }
-  //   `;
-  // }
 };
 
 function queryBuilder(dashboardItems, dashboardStates)
@@ -120,7 +101,7 @@ const styles = StyleSheet.create({
 
 function wrapDynamicGraphQL(ComponentToWrap)
 {
-  class Wrapped extends PureComponent {
+  class DynamicDashboardQueryHOC extends PureComponent {
 
     constructor(props) {
       super(props);
@@ -129,197 +110,209 @@ function wrapDynamicGraphQL(ComponentToWrap)
 
     componentWillReceiveProps(nextProps)
     {
-      if (this.wrapped === null ||
-          this.props.selectedTab !== nextProps.selectedTab ||
-          this.props.dashboardPages[this.props.selectedTab].length !== nextProps.dashboardPages[nextProps.selectedTab].length ||
-          !isEqual(this.props.dashboardPagesStates, nextProps.dashboardPagesStates))
+      const props = this.props;
+
+      if (this.wrapped !== null &&
+          props.selectedTab === nextProps.selectedTab &&
+          isEqual(props.dashboardPages, nextProps.dashboardPages) &&
+          isEqual(props.dashboardPagesStates, nextProps.dashboardPagesStates))
       {
-        const {
-          dashboardPages,
-          dashboardPagesStates,
-          selectedTab,
-        } = nextProps;
-
-        const dashboardItems = dashboardPages[selectedTab];
-        const dashboardStates = dashboardPagesStates[selectedTab];
-
-        if (dashboardItems)
-        {
-          const { query, config } = queryBuilder(dashboardItems, dashboardStates);
-          this.wrapped = graphql(query, config)(ComponentToWrap);
-        }
+        return;
       }
-    }
 
-    componentWillUnmount()
-    {
-      // TODO: is this necessary?
-      this.wrapped = null;
-    }
-
-    addToLayout(identifier)
-    {
       const {
-        createDashboardItem,
-        createDashboardItemLocal,
         dashboardPages,
-        data,
+        dashboardPagesStates,
         selectedTab,
-      } = this.props;
+      } = nextProps;
 
       const dashboardItems = dashboardPages[selectedTab];
-      const arr = computeDashboardFreeValues(dashboardItems);
+      const dashboardStates = dashboardPagesStates[selectedTab];
 
-      if (data.user)
+      if (!dashboardItems || !dashboardStates)
       {
-        // createDashboardItem(
-        //   identifier,
-        //   3,
-        //   3,
-        //   0,
-        //   arr[0],
-        // );
+        return;
       }
-      else
-      {
-        createDashboardItemLocal({
-          id: arr[1],
-          identifier: identifier,
-          w: 3,
-          h: 3,
-          x: 0,
-          y: arr[0],
-        });
-      }
-    }
 
-    removeFromLayout(id)
-    {
-      const {
-        data,
-        destroyDashboardItem,
-        destroyDashboardItemLocal,
-      } = this.props;
-
-      if (data.user)
-      {
-        // destroyDashboardItem(id);
-      }
-      else
-      {
-        destroyDashboardItemLocal(id);
-      }
-    }
-
-    saveLayout(layout)
-    {
-      const {
-        data,
-        dashboardPages,
-        saveDashboardItemsLocal,
-        selectedTab,
-        updateDashboardItems,
-      } = this.props;
-
-      const dashboardItems = dashboardPages[selectedTab];
-
-      var layoutChanged = false;
-      const newDashboardItemsMap = {};
-      layout.forEach((dashboardItem) => {
-        newDashboardItemsMap[dashboardItem.i] = {
-          id: dashboardItem.i,
-          w: dashboardItem.w,
-          h: dashboardItem.h,
-          x: dashboardItem.x,
-          y: dashboardItem.y,
-        };
-      });
-
-      dashboardItems.forEach((dashboardItem) => {
-        const matchItem = newDashboardItemsMap[dashboardItem.id];
-        layoutChanged = layoutChanged || dashboardItem.w != matchItem.w;
-        layoutChanged = layoutChanged || dashboardItem.h != matchItem.h;
-        layoutChanged = layoutChanged || dashboardItem.x != matchItem.x;
-        layoutChanged = layoutChanged || dashboardItem.y != matchItem.y;
-      });
-
-      if (layoutChanged)
-      {
-        if (data.user)
-        {
-          // updateDashboardItems(Object.values(newDashboardItemsMap));
-        }
-        else
-        {
-          dashboardItems.map((item) => {
-            newDashboardItemsMap[item.id].identifier = item.identifier;
-          });
-          saveDashboardItemsLocal(Object.values(newDashboardItemsMap));
-        }
-      }
+      const { query, config } = queryBuilder(dashboardItems, dashboardStates);
+      this.wrapped = graphql(query, config)(ComponentToWrap);
     }
 
     render() {
-      const {
-        changeDashboardItemPlotRange,
-        changeDashboardPageState,
-        changeKeySelectValue,
-        changeScrollActive,
-        changeSelectedTab,
-        changeValueSelectValue,
-        dashboardAction,
-        dashboardData,
-        dashboardPages,
-        dashboardPagesStates,
-        data,
-        keySelectValue,
-        logDashboardActionStart,
-        logDashboardActionStop,
-        nightMode,
-        toggleSidebar,
-        scrollActive,
-        selectedTab,
-        sidebarActive,
-        valueSelectValue,
-      } = this.props;
-
-      const Wrapped = this.wrapped;
-
+      const W = this.wrapped;
       return (
-        <div className={css(styles.container)}>
-          <Dashboard
-            addToLayout={(identifier) => this.addToLayout(identifier)}
-            changeDashboardItemPlotRange={changeDashboardItemPlotRange}
-            changeDashboardPageState={changeDashboardPageState}
-            changeKeySelectValue={changeKeySelectValue}
-            changeScrollActive={changeScrollActive}
-            changeSelectedTab={changeSelectedTab}
-            changeValueSelectValue={changeValueSelectValue}
-            dashboardAction={dashboardAction}
-            dashboardData={dashboardData}
-            dashboardPages={dashboardPages}
-            dashboardPagesStates={dashboardPagesStates}
-            keySelectValue={keySelectValue}
-            nightMode={nightMode}
-            toggleSidebar={toggleSidebar}
-            logDashboardActionStart={logDashboardActionStart}
-            logDashboardActionStop={logDashboardActionStop}
-            removeFromLayout={(id) => this.removeFromLayout(id)}
-            saveLayout={(layout) => this.saveLayout(layout)}
-            selectedTab={selectedTab}
-            scrollActive={scrollActive}
-            sidebarActive={sidebarActive}
-            valueSelectValue={valueSelectValue}
-          />
-          <div className={css(styles.hidden)}>
-            {this.wrapped && <Wrapped />}
-          </div>
+        <div className={css(styles.hidden)}>
+          {this.wrapped && <W />}
         </div>
       );
     }
   }
 
-  return Wrapped;
+  return DynamicDashboardQueryHOC;
 }
 
-export default wrapDynamicGraphQL(WrappedComponent);
+const DataProvider = wrapDynamicGraphQL(WrappedComponent);
+
+class Container extends PureComponent
+{
+  addToLayout(identifier)
+  {
+    const {
+      createDashboardItem,
+      createDashboardItemLocal,
+      dashboardPages,
+      data,
+      selectedTab,
+    } = this.props;
+
+    const dashboardItems = dashboardPages[selectedTab];
+    const arr = computeDashboardFreeValues(dashboardItems);
+
+    if (data.user)
+    {
+      // createDashboardItem(
+      //   identifier,
+      //   3,
+      //   3,
+      //   0,
+      //   arr[0],
+      // );
+    }
+    else
+    {
+      createDashboardItemLocal({
+        id: arr[1],
+        identifier: identifier,
+        w: 3,
+        h: 3,
+        x: 0,
+        y: arr[0],
+      });
+    }
+  }
+
+  removeFromLayout(id)
+  {
+    const {
+      data,
+      destroyDashboardItem,
+      destroyDashboardItemLocal,
+    } = this.props;
+
+    if (data.user)
+    {
+      // destroyDashboardItem(id);
+    }
+    else
+    {
+      destroyDashboardItemLocal(id);
+    }
+  }
+
+  saveLayout(layout)
+  {
+    const {
+      data,
+      dashboardPages,
+      saveDashboardItemsLocal,
+      selectedTab,
+      updateDashboardItems,
+    } = this.props;
+
+    const dashboardItems = dashboardPages[selectedTab];
+
+    var layoutChanged = false;
+    const newDashboardItemsMap = {};
+    layout.forEach((dashboardItem) => {
+      newDashboardItemsMap[dashboardItem.i] = {
+        id: dashboardItem.i,
+        w: dashboardItem.w,
+        h: dashboardItem.h,
+        x: dashboardItem.x,
+        y: dashboardItem.y,
+      };
+    });
+
+    dashboardItems.forEach((dashboardItem) => {
+      const matchItem = newDashboardItemsMap[dashboardItem.id];
+      layoutChanged = layoutChanged || dashboardItem.w != matchItem.w;
+      layoutChanged = layoutChanged || dashboardItem.h != matchItem.h;
+      layoutChanged = layoutChanged || dashboardItem.x != matchItem.x;
+      layoutChanged = layoutChanged || dashboardItem.y != matchItem.y;
+    });
+
+    if (layoutChanged)
+    {
+      if (data.user)
+      {
+        // updateDashboardItems(Object.values(newDashboardItemsMap));
+      }
+      else
+      {
+        dashboardItems.map((item) => {
+          newDashboardItemsMap[item.id].identifier = item.identifier;
+        });
+        saveDashboardItemsLocal(Object.values(newDashboardItemsMap));
+      }
+    }
+  }
+
+  render()
+  {
+    const {
+      changeDashboardItemPlotRange,
+      changeDashboardPageState,
+      changeKeySelectValue,
+      changeScrollActive,
+      changeSelectedTab,
+      changeValueSelectValue,
+      dashboardAction,
+      dashboardData,
+      dashboardPages,
+      dashboardPagesStates,
+      keySelectValue,
+      logDashboardActionStart,
+      logDashboardActionStop,
+      nightMode,
+      toggleSidebar,
+      scrollActive,
+      selectedTab,
+      sidebarActive,
+      valueSelectValue,
+    } = this.props;
+
+    return (
+      <div className={css(styles.container)}>
+        <DataProvider
+          {...this.props}
+        />
+        <Dashboard
+          addToLayout={(identifier) => this.addToLayout(identifier)}
+          changeDashboardItemPlotRange={changeDashboardItemPlotRange}
+          changeDashboardPageState={changeDashboardPageState}
+          changeKeySelectValue={changeKeySelectValue}
+          changeScrollActive={changeScrollActive}
+          changeSelectedTab={changeSelectedTab}
+          changeValueSelectValue={changeValueSelectValue}
+          dashboardAction={dashboardAction}
+          dashboardData={dashboardData}
+          dashboardPages={dashboardPages}
+          dashboardPagesStates={dashboardPagesStates}
+          keySelectValue={keySelectValue}
+          nightMode={nightMode}
+          toggleSidebar={toggleSidebar}
+          logDashboardActionStart={logDashboardActionStart}
+          logDashboardActionStop={logDashboardActionStop}
+          removeFromLayout={(id) => this.removeFromLayout(id)}
+          saveLayout={(layout) => this.saveLayout(layout)}
+          selectedTab={selectedTab}
+          scrollActive={scrollActive}
+          sidebarActive={sidebarActive}
+          valueSelectValue={valueSelectValue}
+        />
+      </div>
+    );
+  }
+}
+
+export default Container;
