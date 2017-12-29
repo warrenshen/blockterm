6fd87cbb9f2551f20839749f699bac9cbfb46c5e
warrenshen <warrenzshen@gmail.com>
2017-12-28 23:24:01 -0800

    Create dashboard page graphql type

--- server/app/graphql/types/user_type.rb
+++ server/app/graphql/types/user_type.rb
@@ -1,15 +1,23 @@
 module Types
   UserType = GraphQL::ObjectType.define do
-  name 'UserType'
+    name 'UserType'
 
-  field :id, !types.ID
-  field :email, !types.String
-  field :dashboardItems, !types[Types::DashboardItemType] do
-    description 'The dashboard items associated with user'
+    field :id, !types.ID
+    field :email, !types.String
 
-    resolve -> (obj, args, ctx) {
-      obj.dashboard_items
-    }
+    field :dashboardItems, !types[Types::DashboardItemType] do
+      description 'The dashboard items associated with user'
+
+      resolve -> (obj, args, ctx) {
+        obj.dashboard_items
+      }
+    end
+    field :dashboardPages, !types[Types::DashboardPageType] do
+      description 'The dashboard pages associated with user'
+
+      resolve -> (obj, args, ctx) {
+        obj.dashboard_pages.order(index: :asc)
+      }
     end
   end
 end
