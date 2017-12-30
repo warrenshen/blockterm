4b9cdf0b1215c40ce5eb66b6927aa5e417ea64b9
Nick Shen <nickzshen@gmail.com>
2017-12-30 03:13:50 -0800

    small styling of notification to match blockterm aesthetic, marqee z-index fix, login page

--- client/src/app/components/Notifications.js
+++ client/src/app/components/Notifications.js
@@ -5,6 +5,7 @@ import PropTypes           from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 import ReactDOM               from 'react-dom';
 import NotificationSystem     from 'react-notification-system';
+import * as STYLES from '../constants/styles';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,13 +13,29 @@ const styles = StyleSheet.create({
   },
 });
 
+const notificationStyles = {
+  NotificationItem: { // Override the notification item
+    DefaultStyle: { // Applied to every notification, regardless of the notification level
+      'borderRadius':'0px',
+      'boxShadow':'none',
+      'borderColor':STYLES.BLAZINGREEN,
+      'fontWeight':'500',
+    },
+
+    success: { // Applied only to the success notification item
+      color: 'red'
+    }
+  }
+}
+
 class Notifications extends Component {
 
   _addNotification(event) {
     event.preventDefault();
     this._notificationSystem.addNotification({
       message: 'Notification message',
-      level: 'success'
+      level: 'success',
+      autoDismiss: 2,
     });
   }
 
@@ -30,7 +47,7 @@ class Notifications extends Component {
     return (
       <div>
         <button onClick={this._addNotification.bind(this)}>Add notification</button>
-        <NotificationSystem ref="notificationSystem" />
+        <NotificationSystem ref="notificationSystem" style={notificationStyles} />
       </div>
       );
   }
