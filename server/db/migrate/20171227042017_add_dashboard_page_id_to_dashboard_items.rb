642d421c0ea6753b145e348ab9a9b5bd3bdf52ce
Nick Shen <nickzshen@gmail.com>
2018-01-01 11:04:27 -0800

    Fix class name in old migration

--- server/db/migrate/20171227042017_add_dashboard_page_id_to_dashboard_items.rb
+++ server/db/migrate/20171227042017_add_dashboard_page_id_to_dashboard_items.rb
@@ -1,4 +1,4 @@
-class AddDashboardPageIdToDashboardItem < ActiveRecord::Migration[5.0]
+class AddDashboardPageIdToDashboardItems < ActiveRecord::Migration[5.0]
   def change
     add_reference :dashboard_items, :dashboard_page, foreign_key: true, null: false
   end
