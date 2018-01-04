022be33a5ebaae57d75e45e86bb981b05848beb8
warrenshen <warrenzshen@gmail.com>
2018-01-04 00:14:20 -0800

    Annotate token model and add to token seeds

--- server/app/models/token.rb
+++ server/app/models/token.rb
@@ -2,13 +2,23 @@
 #
 # Table name: tokens
 #
-#  id         :integer          not null, primary key
-#  short_name :string           not null
-#  long_name  :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  image_url  :string           default(""), not null
-#  website    :string           default(""), not null
+#  id                 :integer          not null, primary key
+#  short_name         :string           not null
+#  long_name          :string           not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_url          :string           default(""), not null
+#  website            :string           default(""), not null
+#  price_usd          :decimal(, )      default(0.0), not null
+#  price_btc          :decimal(, )      default(0.0), not null
+#  volume_usd_24h     :decimal(, )      default(0.0), not null
+#  market_cap_usd     :decimal(, )      default(0.0), not null
+#  available_supply   :decimal(, )      default(0.0), not null
+#  total_supply       :decimal(, )      default(0.0), not null
+#  max_supply         :decimal(, )      default(0.0), not null
+#  percent_change_1h  :decimal(, )      default(0.0), not null
+#  percent_change_24h :decimal(, )      default(0.0), not null
+#  percent_change_7d  :decimal(, )      default(0.0), not null
 #
 # Indexes
 #
