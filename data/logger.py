02e4b81a86912512f7ad9c3289a0cea103d0b80a
warrenshen <warrenzshen@gmail.com>
2018-01-06 06:58:48 +0000

    Log logs to logs directory

--- data/logger.py
+++ data/logger.py
@@ -3,7 +3,7 @@ import logging
 from configs import SRC_PATH
 from utils import unix_timestamp_today
 
-log_path = '{}/log_{}.log'.format(SRC_PATH, unix_timestamp_today())
+log_path = '{}/logs/log_{}.log'.format(SRC_PATH, unix_timestamp_today())
 
 formatter = logging.Formatter(fmt='%(asctime)-15s %(levelname)-8s %(message)s')
 handler = logging.FileHandler(log_path, 'a+')
