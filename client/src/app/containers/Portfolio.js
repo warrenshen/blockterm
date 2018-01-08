97cd2413aaf6d698f16cbf739897a3ad054ebfbd
warrenshen <warrenzshen@gmail.com>
2018-01-07 20:04:44 -0800

    Get basic update token users with save button to work

--- client/src/app/containers/Portfolio.js
+++ client/src/app/containers/Portfolio.js
@@ -5,8 +5,11 @@ import { bindActionCreators } from 'redux';
 import { compose, graphql }   from 'react-apollo';
 import {
   TokenUsersQuery,
+  UpdateTokenUsersMutation,
+  UpdateTokenUsersMutationOptions,
 }                             from '../queries';
 import Portfolio              from '../views/Portfolio';
+import * as portfolioActions  from '../redux/modules/portfolio';
 
 /* -----------------------------------------
   Redux
@@ -15,12 +18,14 @@ import Portfolio              from '../views/Portfolio';
 const mapStateToProps = (state) => {
   return {
     nightMode: state.globals.nightMode,
+    tokenUsers: state.portfolio.tokenUsers,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(
     {
+      changeTokenUserAmount: portfolioActions.changeTokenUserAmount,
     },
     dispatch
   );
@@ -28,5 +33,6 @@ const mapDispatchToProps = (dispatch) => {
 
 export default compose(
   graphql(TokenUsersQuery),
+  graphql(UpdateTokenUsersMutation, UpdateTokenUsersMutationOptions),
   connect(mapStateToProps, mapDispatchToProps),
 )(Portfolio);
