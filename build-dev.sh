6b1caba0ce29905efad4e4e428f7654a3fd28b4c
Bob <bob@houzz.com>
2018-01-09 20:05:41 -0800

    Change deployment config for a new project
    Summary:
      + Change from blockterm to block
      + Use helm to do infra deployment for kube-lego and nginx-ingress-controller
      + Change ingress to use nginx instead of GCE
      + Use regional ip instead of global for nginx loadbalancer, abandoning ingress GCE controller

--- build-dev.sh
+++ build-dev.sh
@@ -1,8 +1,8 @@
 echo "Building docker images for client"
-echo "docker build -f deployment/Dockerfile.client -t us.gcr.io/blockterm-188823/client ."
-docker build -f deployment/Dockerfile.client -t us.gcr.io/blockterm-188823/client .
+echo "docker build -f deployment/Dockerfile.client -t us.gcr.io/block-191602/client ."
+docker build -f deployment/Dockerfile.client -t us.gcr.io/block-191602/client .
 
 echo "Building docker images for server"
-echo "docker build -f deployment/Dockerfile.server -t us.gcr.io/blockterm-188823/server ."
-docker build -f deployment/Dockerfile.server -t us.gcr.io/blockterm-188823/server .
+echo "docker build -f deployment/Dockerfile.server -t us.gcr.io/block-191602/server ."
+docker build -f deployment/Dockerfile.server -t us.gcr.io/block-191602/server .
 
