c30b72031cd7fbfd0b6a240818cff863aab9aa15
warrenshen <warrenzshen@gmail.com>
2018-01-12 15:06:17 -0800

    Switch prod api url to http

--- client/src/app/config/index.js
+++ client/src/app/config/index.js
@@ -3,7 +3,7 @@
 // Note that NODE_ENV is set in package.json.
 const API_URL = process.env.NODE_ENV == 'dev' ?
                'http://localhost:9999/graphql' :
-               'https://api.blockterm.com/graphql';
+               'http://api.blockterm.com/graphql';
 
 export const appConfig = {
   apollo: {
