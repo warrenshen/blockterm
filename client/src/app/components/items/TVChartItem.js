1c84a0b6e20aef2c56e2e80372a60b9ef23a65dd
warrenshen <warrenzshen@gmail.com>
2018-01-16 21:36:17 -0800

    Set default time interval to 5 minutes

--- client/src/app/components/items/TVChartItem.js
+++ client/src/app/components/items/TVChartItem.js
@@ -68,7 +68,7 @@ class TVChartItem extends PureComponent
     const url =
       'https://s.tradingview.com/widgetembed/?' +
       `symbol=${value}&` +
-      'interval=3&' +
+      'interval=5&' +
       'withdateranges=1&' +
       'hideideas=1&' +
       'hidesidetoolbar=1&' +
