0d3ff9d1c3fc24d6e3895dfe7e13dc74344392bd
warrenshen <warrenzshen@gmail.com>
2018-01-18 22:55:10 -0800

    Beef up coinmarketcap client global mode with percent dominance and bitcoin total market cap info

--- data/coinmarketcap_client.py
+++ data/coinmarketcap_client.py
@@ -24,34 +24,89 @@ class CoinmarketcapClient:
     return result
 
   def _get_global(self):
-    target_url = 'https://api.coinmarketcap.com/v1/global/'
-    result = self._curl_result(target_url)
+    global_target_url = 'https://api.coinmarketcap.com/v1/global/'
+    global_result = self._curl_result(global_target_url)
 
-    if result == None:
+    if global_result == None:
       logger.info('Something went wrong with fetching globals')
-      return
-    elif 'total_market_cap_usd' not in result:
-      logger.info('Globals response different than expected: {}'.format(result))
-      return
+      raise Exception('Something went wrong with fetching globals')
+    elif 'total_market_cap_usd' not in global_result:
+      logger.info('Globals response different than expected: {}'.format(global_result))
+      raise Exception('Globals response different than expected: {}'.format(global_result))
 
-    value = result['total_market_cap_usd']
-    timestamp = result['last_updated']
-    datetime_string = unix_timestamp_to_datetime_string(timestamp)
+    bitcoin_target_url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin'
+    bitcoin_result = self._curl_result(bitcoin_target_url)
+
+    if bitcoin_result == None or len(bitcoin_result) <= 0:
+      raise Exception('Something went wrong with fetching bitcoin ticker')
+
+    ethereum_target_url = 'https://api.coinmarketcap.com/v1/ticker/ethereum'
+    ethereum_result = self._curl_result(ethereum_target_url)
+
+    if ethereum_result == None or len(ethereum_result) <= 0:
+      raise Exception('Something went wrong with fetching bitcoin ticker')
+
+    total_market_cap_usd = global_result['total_market_cap_usd']
+    global_timestamp = global_result['last_updated']
+    datetime_string = unix_timestamp_to_datetime_string(global_timestamp)
 
     db = SQLite3Database('total_market_caps.db')
-    db.insert_total_ticker(str(result), timestamp)
+    db.insert_total_ticker(str(global_result), global_timestamp)
     db.close()
 
     response = self.api.create_market_ticker_by_market_name(
       'TOTAL',
-      value,
+      total_market_cap_usd,
       datetime_string
     )
+
     if 'errors' in response:
       logger.info('Something went wrong with saving total market ticker')
-      return False
-    else:
-      return True
+      raise Exception('Something went wrong with saving total market ticker')
+
+    ethereum_market_cap_usd = ethereum_result[0]['market_cap_usd']
+    ethereum_timestamp = ethereum_result[0]['last_updated']
+    datetime_string = unix_timestamp_to_datetime_string(int(ethereum_timestamp))
+
+    bitcoin_market_cap_usd = bitcoin_result[0]['market_cap_usd']
+    bitcoin_timestamp = bitcoin_result[0]['last_updated']
+    datetime_string = unix_timestamp_to_datetime_string(int(bitcoin_timestamp))
+
+    response = self.api.create_market_ticker_by_market_name(
+      'TOTAL_BITCOIN',
+      bitcoin_market_cap_usd,
+      datetime_string
+    )
+
+    # TODO: total ethereum?
+
+    altcoins_market_cap_usd = str(int(total_market_cap_usd) - int(bitcoin_market_cap_usd))
+    response = self.api.create_market_ticker_by_market_name(
+      'TOTAL_ALTCOINS',
+      altcoins_market_cap_usd,
+      datetime_string
+    )
+
+    bitcoin_percent = round(int(bitcoin_market_cap_usd) * 100 / int(total_market_cap_usd), 2)
+    response = self.api.create_market_ticker_by_market_name(
+      'PERCENT_BITCOIN',
+      str(bitcoin_percent),
+      datetime_string
+    )
+
+    ethereum_percent = round(int(ethereum_market_cap_usd) * 100 / int(total_market_cap_usd), 2)
+    response = self.api.create_market_ticker_by_market_name(
+      'PERCENT_ETHEREUM',
+      str(ethereum_percent),
+      datetime_string
+    )
+
+    altcoins_percent = round(100.0 - bitcoin_percent - ethereum_percent, 2)
+    response = self.api.create_market_ticker_by_market_name(
+      'PERCENT_ALTCOINS',
+      str(altcoins_percent),
+      datetime_string
+    )
 
   def _get_tickers(self):
     target_url = 'https://api.coinmarketcap.com/v1/ticker/?limit=0'
