f9ff1bf6b3793bd1411c6ac8411c0ac55b2787e2
warrenshen <warrenzshen@gmail.com>
2018-01-19 17:57:41 -0800

    Support apollo request batching - woo!

--- client/src/app/services/apollo/index.js
+++ client/src/app/services/apollo/index.js
@@ -8,19 +8,43 @@ import {
 
 import {
   ApolloClient,
+  createBatchingNetworkInterface,
   createNetworkInterface,
   addTypename,
 }                         from 'react-apollo';
 import { appConfig }      from '../../config';
 
-const networkInterface = createNetworkInterface({
+// const networkInterface = createNetworkInterface({
+//   // connectToDevTools: true,
+//   // transportBatching: true,
+//   uri: appConfig.apollo.networkInterface,
+// });
+
+// networkInterface.use([{
+//   applyMiddleware(req, next) {
+//     if (!req.options.headers) {
+//       req.options.headers = {};  // Create the header object if needed.
+//     }
+
+//     // Get headers from local storage.
+//     const authToken = getItem(AUTH_TOKEN_COOKIE);
+//     const timeZone = getItem(timeZone);
+//     req.options.headers.authorization = authToken ? `Bearer ${authToken}` : null;
+//     req.options.headers['X-Time-Zone'] = timeZone ? timeZone : moment.tz.guess();
+
+//     next();
+//   }
+// }]);
+
+const networkInterface = createBatchingNetworkInterface({
   // connectToDevTools: true,
   // transportBatching: true,
+  batchInterval: 50,  // 50 milliseconds.
   uri: appConfig.apollo.networkInterface,
 });
 
 networkInterface.use([{
-  applyMiddleware(req, next) {
+  applyBatchMiddleware(req, next) {
     if (!req.options.headers) {
       req.options.headers = {};  // Create the header object if needed.
     }
@@ -36,5 +60,3 @@ networkInterface.use([{
 }]);
 
 export const apolloClient = new ApolloClient({ networkInterface });
-
-export default apolloClient;
