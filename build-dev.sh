8a1a70c170ab1e62d6b8806223b6d0c02ee76f2d
Bob <bob@houzz.com>
2018-01-26 18:37:25 -0800

    Add cors and change docker compose
    Summary:
      + Add cors Dockerfile for deployment
      + Change build-dev.sh to build the cors images
      + Change docker-compose to use local volumes instead of mounting the folder in a hacky way.

--- build-dev.sh
+++ build-dev.sh
@@ -6,3 +6,6 @@ echo "Building docker images for server"
 echo "docker build -f deployment/Dockerfile.server -t us.gcr.io/block-191602/server ."
 docker build -f deployment/Dockerfile.server -t us.gcr.io/block-191602/server .
 
+echo "Building docker images for cors"
+echo "docker build -f deployment/Dockerfile.cors -t us.gcr.io/block-191602/cors ."
+docker build -f deployment/Dockerfile.cors -t us.gcr.io/block-191602/cors .
