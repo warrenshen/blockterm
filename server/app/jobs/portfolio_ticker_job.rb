1d26abd2bc030f84d6beea86c0dc311234438a00
warrenshen <warrenzshen@gmail.com>
2018-02-03 00:02:31 -0800

    Create portfolio ticker type, whenever schedule, and portfolio ticker job

---
+++ server/app/jobs/portfolio_ticker_job.rb
@@ -0,0 +1,42 @@
+class PortfolioTickerJob < ApplicationJob
+
+  def perform(*args)
+    users_with_portfolio = User.where_exists(:token_users)
+    users_with_portfolio.each do |user|
+      token_users = TokenUser.includes(:token).where(user_id: user.id)
+      PortfolioTicker.create(
+        user_id: user.id,
+        value_usd: calculate_value_usd(token_users),
+        value_btc: calculate_value_btc(token_users),
+        value_eth: calculate_value_eth(token_users),
+        timestamp: DateTime.now
+      )
+    end
+  end
+
+  def calculate_value_usd(token_users)
+    value_usd = 0.0
+    token_users.each do |token_user|
+      value_usd += token_user.amount * token_user.token.price_usd
+    end
+    value_usd
+  end
+
+  def calculate_value_btc(token_users)
+    value_btc = 0.0
+    token_users.each do |token_user|
+      value_btc += token_user.amount * token_user.token.price_btc
+    end
+    value_btc
+  end
+
+  def calculate_value_eth(token_users)
+    value_eth = 0.0
+    token_eth = Token.find_by_identifier('ethereum')
+    token_users.each do |token_user|
+      eth_price = token_user.token.price_usd / token_eth.price_usd
+      value_eth += token_user.amount * eth_price
+    end
+    value_eth
+  end
+end
---
+++ server/config/schedule.rb
@@ -0,0 +1,23 @@
+# Use this file to easily define all of your cron jobs.
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+ENV.each { |k, v| env(k, v) }
+
+set :job_template, "/bin/bash -c ':job'"
+set :output,       '/blockterm/log/cron.log'
+
+job_type :job, 'cd /blockterm && bundle exec rails runner :task :output'
+
+every 6.hours do
+  job 'PortfolioTickerJob.perform_now'
+end
