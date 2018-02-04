1ce9d90476b9ef6aef875a948ecef8c98a4334c7
warrenshen <warrenzshen@gmail.com>
2018-02-03 17:30:31 -0800

    Install prerender package and use it in production server

--- client/src/server/SPA/index.js
+++ client/src/server/SPA/index.js
@@ -6,6 +6,7 @@ const chalk           = require('chalk');
 const express         = require('express');
 const fs              = require('fs');
 const path            = require('path');
+const prerender       = require('prerender-node');
 
 const app       = express();
 const DOCS_PATH = '../../../docs/';
@@ -16,6 +17,8 @@ app.set('port', PORT);
 app.set('ipAdress', IP_ADRESS);
 
 app.use(express.static(path.join(__dirname, DOCS_PATH)));
+// Production only: use prerender middleware.
+app.use(prerender.set('prerenderToken', 'rd7gKVjJ6BxewgQ2nKqm'));
 
 app.get('/health', (req, res) => res.sendStatus(200));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, DOCS_PATH, 'index.html')));
