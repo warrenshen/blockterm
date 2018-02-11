202ecb405e23b079aff756acca708eddf5c0d4c4
warrenshen <warrenzshen@gmail.com>
2018-02-10 18:23:14 -0800

    Create api keys page to add exchange keys

---
+++ client/src/app/containers/ApiKeys.js
@@ -0,0 +1,45 @@
+// @flow weak
+
+import { connect }            from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { compose, graphql }   from 'react-apollo';
+import {
+  error as createNotificationError,
+  success as createNotificationSuccess,
+}                             from 'react-notification-system-redux';
+import {
+  CreateExchangeKeyMutation,
+  CreateExchangeKeyMutationOptions,
+}                             from '../queries';
+import ApiKeys                from '../views/ApiKeys';
+import * as balancesActions   from '../redux/modules/balances';
+
+/* -----------------------------------------
+  Redux
+ ------------------------------------------*/
+
+const mapStateToProps = (state) => ({
+  apiKey: state.balances.apiKey,
+  exchange: state.balances.exchange,
+  exchangeKeys: state.balances.exchangeKeys,
+  secretKey: state.balances.secretKey,
+  user: state.globals.user,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      changeApiKey: balancesActions.changeApiKey,
+      changeExchange: balancesActions.changeExchange,
+      changeSecretKey: balancesActions.changeSecretKey,
+      createNotificationError: createNotificationError,
+      createNotificationSuccess: createNotificationSuccess,
+    },
+    dispatch
+  );
+};
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  graphql(CreateExchangeKeyMutation, CreateExchangeKeyMutationOptions),
+)(ApiKeys);
--- client/src/app/redux/modules/balances.js
+++ client/src/app/redux/modules/balances.js
@@ -4,12 +4,19 @@
   constants
  ------------------------------------------*/
 const APOLLO_QUERY_RESULT = 'APOLLO_QUERY_RESULT';
+const CHANGE_API_KEY = 'CHANGE_API_KEY';
+const CHANGE_EXCHANGE = 'CHANGE_EXCHANGE';
+const CHANGE_SECRET_KEY = 'CHANGE_SECRET_KEY';
 
 /* -----------------------------------------
   Reducer
  ------------------------------------------*/
 const initialState = {
+  apiKey: '',
+  error: null,
+  exchange: null,
   exchangeKeys: [],
+  secretKey: '',
 };
 
 export default function(state = initialState, action)
@@ -27,7 +34,46 @@ export default function(state = initialState, action)
         default:
           return state;
       }
+    case CHANGE_API_KEY:
+      return {
+        ...state,
+        apiKey: action.apiKey,
+      };
+    case CHANGE_EXCHANGE:
+      return {
+        ...state,
+        exchange: action.exchange,
+      };
+    case CHANGE_SECRET_KEY:
+      return {
+        ...state,
+        secretKey: action.secretKey,
+      };
     default:
       return state;
   }
+}
+
+export function changeApiKey(apiKey)
+{
+  return {
+    apiKey: apiKey,
+    type: CHANGE_API_KEY,
+  };
+}
+
+export function changeExchange(exchange)
+{
+  return {
+    exchange: exchange,
+    type: CHANGE_EXCHANGE,
+  };
+}
+
+export function changeSecretKey(secretKey)
+{
+  return {
+    secretKey: secretKey,
+    type: CHANGE_SECRET_KEY,
+  };
 }
