6ccdcd08c9689022fd7203e092d9287362e8b28b
warrenshen <warrenzshen@gmail.com>
2018-02-17 09:36:44 -0800

    Fix twitter item component style

--- client/src/app/components/items/TwitterItem.js
+++ client/src/app/components/items/TwitterItem.js
@@ -13,16 +13,10 @@ import {
 } from '../../constants/items.js';
 
 const styles = StyleSheet.create({
-  // The combination of flexDirection: 'row' and alignItems: 'stretch'
-  // allow us to force the child of the container div to stretch to full width.
-  // This is necessary because the Timeline component has an extra div
-  // wrapping the twitter iframe, which we want to stretch to match the height
-  // of the container div (but don't have an easy way to add flex: 1 to it).
   container: {
     flex: '1',
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'stretch',
     width: '100%',
   },
   noPointerEvents: {
@@ -50,18 +44,27 @@ class TwitterItem extends PureComponent
     const dataSource = value in TWITTER_VALUE_TO_DATA_SOURCE ?
                        TWITTER_VALUE_TO_DATA_SOURCE[value] :
                        TWITTER_VALUE_TO_DATA_SOURCE[TWITTER_VALUE_SEARCH_CRYPTOCURRENCY];
-
+    const onLoad = (event) => {
+      // This is necessary because the Timeline component has an extra div
+      // wrapping the twitter iframe, which we want to stretch to match the height
+      // of the container div (but don't have an easier way to add flex: 1 to it).
+      this.instance.firstChild.style = 'flex: 1';
+    };
     return (
-      <div className={css(styles.container, dashboardAction && styles.noPointerEvents)}>
+      <div
+        className={css(styles.container, dashboardAction && styles.noPointerEvents)}
+        ref={(el) => this.instance = el}
+      >
         <Timeline
           dataSource={dataSource}
           options={{
             chrome: 'noheader nofooter transparent',
+            width: '100%',
             height: '100%',
             theme: nightMode ? 'dark' : 'light',
             transparent: true,
           }}
-          // onLoad={() => console.log('Timeline is loaded!')}
+          onLoad={onLoad}
         />
       </div>
     );
