2d545374a40efde547b437bf2c878d9d5063da33
warrenshen <warrenzshen@gmail.com>
2018-02-19 17:59:12 -0800

    Get kucoin candle chart pseudo subscriptions (polling) working

--- client/src/app/components/items/TVChartItem.js
+++ client/src/app/components/items/TVChartItem.js
@@ -66,7 +66,8 @@ function getKucoin()
   }
   return kucoin;
 }
-async function fetchOHLCVTicker(symbol, fromUnixTimestamp, toUnixTimestamp, resolution)
+
+async function fetchOHLCVTickers(symbol, fromUnix, toUnix, resolution)
 {
   const kucoin = getKucoin();
 
@@ -78,8 +79,8 @@ async function fetchOHLCVTicker(symbol, fromUnixTimestamp, toUnixTimestamp, reso
       undefined,
       undefined,
       {
-        from: fromUnixTimestamp,
-        to: toUnixTimestamp,
+        from: fromUnix,
+        to: toUnix,
         resolution: resolution,
       },
     );
@@ -89,6 +90,26 @@ async function fetchOHLCVTicker(symbol, fromUnixTimestamp, toUnixTimestamp, reso
   {
     console.log(error);
   }
+}
+
+async function fetchFormattedOHLCVTickers(symbol, fromUnix, toUnix, resolution)
+{
+  const response = await fetchOHLCVTickers(
+    symbol,
+    fromUnix,
+    toUnix,
+    resolution,
+  );
+  return response.map(
+    ([time, open, high, low, close, volume]) => ({
+      time: time,
+      open,
+      high,
+      low,
+      close,
+      volume,
+    })
+  );
 }
 
 class TVChartItem extends PureComponent
@@ -107,7 +128,8 @@ class TVChartItem extends PureComponent
   constructor(props)
   {
     super(props);
-    this.uid = uniqueId('tv-chart-item-');
+    this.uId = uniqueId('tv-chart-item-');
+    this.apiId = null;
   }
 
   componentDidMount()
@@ -132,121 +154,164 @@ class TVChartItem extends PureComponent
       value,
     } = this.props;
 
-    const datafeed = {
-      onReady: (callback) => {
-        if (this.instance)
-        {
-          this.instance.firstChild.style = 'width: 100%; height: 100%;';
-        }
-        setTimeout(
-          () => callback({
-            supports_search: false,
-            supported_resolutions: ['1', '5', '15', '30', '60', '480', '1D', '1W', '1M'],
-            supports_time: true,
-          }),
-          0,
-        );
-      },
-      resolveSymbol: (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) => {
-        setTimeout(
-          onSymbolResolvedCallback({
-            name: 'name',
-            ticker: 'ticker',
-            description: 'description',
-            type: 'bitcoin',
-            session: '24x7',
-            timezone: 'America/Los_Angeles',
-            minmov: 1,
-            pricescale: 100000000,
+    if (value.indexOf('KUCOIN:') === 0)
+    {
+      const symbol = value.split(':', 2)[1];
 
-            has_intraday: true,
-            has_daily: true,
-            has_weekly_and_monthly: true,
-            has_empty_bars: true,
-            supported_resolutions: ['1', '5', '15', '30', '60', '480', '1D', '1W', '1M'],
-          }),
-          0,
-        );
-      },
-      getBars: async (
-        symbolInfo,
-        resolution,
-        fromUnixTimestamp,
-        toUnixTimestamp,
-        onHistoryCallback,
-        onErrorCallback,
-        firstDataRequest,
-      ) => {
-        const rawResponse = await fetchOHLCVTicker(
-          'KCS/BTC',
+      const datafeed = {
+        onReady: (callback) => {
+          if (this.instance)
+          {
+            this.instance.firstChild.style = 'width: 100%; height: 100%;';
+          }
+          setTimeout(
+            () => callback({
+              supported_resolutions: ['1', '5', '15', '30', '60', '480', '1D', '1W', '1M'],
+              supports_mark: false,
+              supports_search: false,
+              supports_time: true,
+            }),
+            0,
+          );
+        },
+        resolveSymbol: (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) => {
+          setTimeout(
+            () => onSymbolResolvedCallback({
+              name: symbolName,
+              ticker: symbolName,
+              description: symbolName,
+              type: 'bitcoin',
+              session: '24x7',
+              timezone: 'America/Los_Angeles',
+              minmov: 1,
+              pricescale: 100000000,
+
+              has_intraday: true,
+              has_daily: true,
+              has_weekly_and_monthly: true,
+              has_empty_bars: true,
+              supported_resolutions: ['1', '5', '15', '30', '60', '480', '1D', '1W', '1M'],
+            }),
+            0,
+          );
+        },
+        getBars: async (
+          symbolInfo,
+          resolution,
           fromUnixTimestamp,
           toUnixTimestamp,
+          onHistoryCallback,
+          onErrorCallback,
+          firstDataRequest,
+        ) => {
+          const formattedResponse = await fetchFormattedOHLCVTickers(
+            symbol,
+            fromUnixTimestamp,
+            toUnixTimestamp,
+            resolution,
+          );
+          onHistoryCallback(formattedResponse);
+        },
+        subscribeBars: (
+          symbolInfo,
           resolution,
-        );
-        const formattedResponse = rawResponse.map(
-          ([time, open, high, low, close, volume]) => ({
-            time: time,
-            open,
-            high,
-            low,
-            close,
-            volume,
-          })
-        );
-        onHistoryCallback(formattedResponse);
-      },
-      subscribeBars: (
-        symbolInfo,
-        resolution,
-        onRealtimeCallback,
-        subscriberUID,
-        onResetCacheNeededCallback,
-      ) => {
-        console.log('subscribeBars');
-        console.log(symbolInfo);
-        console.log(resolution);
-      },
-      unsubscribeBars: (subscriberUID) => { console.log('unsubscribeBars') },
-    };
+          onRealtimeCallback,
+          subscriberUID,
+          onResetCacheNeededCallback,
+        ) => {
+          if (this.apiId !== null)
+          {
+            console.log('bad');
+          }
 
-    const widgetConfig = {
-      debug: true,
-      fullscreen: true,
-      symbol: 'AAPL',
-      interval: 'D',
-      container_id: `${this.uid}`,
-      datafeed: datafeed,
-      library_path: "/",
-      locale: "en",
-      //  Regression Trend-related functionality is not implemented yet, so it's hidden for a while
-      drawings_access: { type: 'black', tools: [ { name: "Regression Trend" } ] },
-      disabled_features: [
-        'go_to_date',
-        'header_compare',
-        'header_fullscreen_button',
-        'header_screenshot',
-        'header_settings',
-        'header_symbol_search',
-        'header_undo_redo',
-        'left_toolbar',
-        // 'symbol_info',
-        'use_localstorage_for_settings',
-      ],
-      // enabled_features: ["study_templates"],
-      // charts_storage_url: 'http://saveload.tradingview.com',
-      //           charts_storage_api_version: "1.1",
-      // client_id: 'tradingview.com',
-      // user_id: 'public_user_id'
-      overrides: {
-        "paneProperties.background": "#222222",
-                    "paneProperties.vertGridProperties.color": "#454545",
-                    "paneProperties.horzGridProperties.color": "#454545",
-        "symbolWatermarkProperties.transparency": 90,
-        "scalesProperties.textColor" : "#AAA"
-      },
-    };
+          this.apiId = setInterval(
+            async () => {
+              const toUnix = moment.utc().unix();
+              let fromUnix;
 
-    var widget = new window.TradingView.widget(widgetConfig);
+              switch (resolution)
+              {
+                case '1':
+                  fromUnix = toUnix - 60;
+                  break;
+                case '60':
+                  fromUnix = toUnix - 60 * 60;
+                  break;
+                case 'D':
+                  fromUnix = toUnix - 24 * 60 * 60;
+                  break;
+                case 'W':
+                  fromUnix = toUnix - 7 * 24 * 60 * 60;
+                  break;
+                default:
+                  console.log('Unsupported resolution!!!!');
+                  break;
+              }
+
+              const formattedResponse = await fetchFormattedOHLCVTickers(
+                symbol,
+                fromUnix,
+                toUnix,
+                resolution,
+              );
+              if (formattedResponse.length > 0)
+              {
+                onRealtimeCallback(formattedResponse[0]);
+              }
+            },
+            4096,
+          );
+        },
+        unsubscribeBars: (subscriberUID) => {
+          if (this.apiId === null)
+          {
+            console.log('bad bad');
+          }
+
+          clearInterval(this.apiId);
+          this.apiId = null;
+        },
+      };
+
+      const widgetConfig = {
+        debug: true,
+        fullscreen: true,
+        symbol: symbol,
+        interval: '1D',
+        container_id: `${this.uId}`,
+        datafeed: datafeed,
+        library_path: "/",
+        locale: "en",
+        //  Regression Trend-related functionality is not implemented yet, so it's hidden for a while
+        drawings_access: { type: 'black', tools: [ { name: "Regression Trend" } ] },
+        disabled_features: [
+          'go_to_date',
+          'header_compare',
+          'header_fullscreen_button',
+          'header_screenshot',
+          'header_settings',
+          'header_symbol_search',
+          'header_undo_redo',
+          'left_toolbar',
+          // 'symbol_info',
+          'use_localstorage_for_settings',
+        ],
+        // enabled_features: ["study_templates"],
+        // charts_storage_url: 'http://saveload.tradingview.com',
+        //           charts_storage_api_version: "1.1",
+        // client_id: 'tradingview.com',
+        // user_id: 'public_user_id'
+        overrides: {
+          "paneProperties.background": "#222222",
+                      "paneProperties.vertGridProperties.color": "#454545",
+                      "paneProperties.horzGridProperties.color": "#454545",
+          "symbolWatermarkProperties.transparency": 90,
+          "scalesProperties.textColor" : "#AAA"
+        },
+      };
+
+      var widget = new window.TradingView.widget(widgetConfig);
+    }
   }
 
   renderExtras()
@@ -320,7 +385,17 @@ class TVChartItem extends PureComponent
       changeModalState,
     } = this.props;
 
-    if (false)
+    if (value.indexOf('KUCOIN:') === 0)
+    {
+      return (
+        <div
+          className={css(styles.frame, nightMode && styles.nightFrame)}
+          id={this.uId}
+          ref={(el) => this.instance = el}
+        />
+      );
+    }
+    else
     {
       const url =
         'https://s.tradingview.com/widgetembed/?' +
@@ -341,16 +416,6 @@ class TVChartItem extends PureComponent
           className={css(styles.frame, nightMode && styles.nightFrame)}
           scrolling="no"
           src={url}
-        />
-      );
-    }
-    else
-    {
-      return (
-        <div
-          className={css(styles.frame, nightMode && styles.nightFrame)}
-          id={this.uid}
-          ref={(el) => this.instance = el}
         />
       );
     }
